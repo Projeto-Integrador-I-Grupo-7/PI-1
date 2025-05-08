b81a262bd71dbd58d4f76865a41caad7e9cdcdce
Daniel Heber da Silva Lima Junior <83608888+Daniel-H-1@users.noreply.github.com>
2025-05-08 00:20:46 -0300

    Add files via upload

---
+++ Aplicação WEB - Problemas Urbanos/contact/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from contact import views
+#from .views import criar_solicitacao
+
+app_name = 'contact'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contact/mapa/', views.mapa, name='mapa'),  
+    path('contact/solicitar/', views.solicitar, name='solicitar'),
+    path('contact/sucesso/', views.pagina_sucesso, name='pagina_sucesso'), 
+]
